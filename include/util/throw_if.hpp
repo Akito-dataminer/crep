c5128561f8cefcb26292a202044419f4e7b18f49
Akito Dataminer <akito.dataminer@gmail.com>
2023-08-10 20:10:46 +0900

    add - throw_if function

---
+++ include/util/throw_if.hpp
@@ -0,0 +1,25 @@
+/*****************************
+ * util/throw_if.hpp
+ *****************************/
+
+#pragma once
+
+#include <functional>
+#include <stdexcept>
+#include <string>
+#include <concepts>
+#include <utility>
+
+namespace util {
+
+template<typename T>
+concept OneOfException = std::is_base_of_v<std::exception, T>;
+
+template <OneOfException T>
+void throw_if( std::function<bool( void )> throw_condition, std::string const &error_message ) {
+  if ( throw_condition ) {
+    throw T( error_message );
+  }
+}
+
+}  // namespace util
