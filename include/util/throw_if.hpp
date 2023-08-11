2d89a54fa0e9b774a456d4e884acff696bcb56ff
Akito Dataminer <akito.dataminer@gmail.com>
2023-08-11 16:25:19 +0900

    remove - throw_if for std::function

--- include/util/throw_if.hpp
+++ include/util/throw_if.hpp
@@ -4,7 +4,6 @@
 
 #pragma once
 
-#include <functional>
 #include <stdexcept>
 #include <string>
 #include <concepts>
@@ -14,13 +13,6 @@ namespace util {
 
 template<typename T>
 concept OneOfException = std::is_base_of_v<std::exception, T>;
-
-template <OneOfException T>
-void throw_if( std::function<bool( void )> throw_condition, std::string const &error_message ) {
-  if ( throw_condition() ) {
-    throw T( error_message );
-  }
-}
 
 template <OneOfException T>
 void throw_if( bool const throw_condition, std::string const &error_message ) {
