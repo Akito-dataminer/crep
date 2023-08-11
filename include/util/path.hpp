d17cdb0d53fb325a8b69428051ab2329672ddf0f
Akito Dataminer <akito.dataminer@gmail.com>
2023-08-11 15:57:19 +0900

    add - joint class

--- include/util/path.hpp
+++ include/util/path.hpp
@@ -4,12 +4,73 @@
 
 #pragma once
 
+#include "config/config.hpp"
+
+#include <concepts>
+#include <functional>
 #include <string>
+#include <type_traits>
 #include <vector>
 
 namespace path {
 
 void recursive_scan_directory( std::string const &scanning_path, std::vector<std::string> &paths );
 std::vector<std::string> recursive_scan_directory( std::string const &scanning_path );
+
+class joint;
+
+template <typename T>
+concept convirtible_to_joint = std::is_convertible_v<T, joint>;
+
+class joint {
+public:
+  explicit joint() = delete;
+  joint( std::string const & );
+  joint( joint const & ) = default;
+  joint &operator=( joint const & ) = default;
+  joint( joint && ) noexcept = default;
+  joint &operator=( joint && ) noexcept = default;
+  ~joint() = default;
+
+  [[deprecated]] void modify( std::function<void ( std::string & )> );
+  std::string to_string() noexcept { return path_element_; }
+  std::string const & to_string() const noexcept { return path_element_; }
+
+  template<convirtible_to_joint Jointable>
+  inline joint& operator+= ( Jointable const &rhs ) noexcept {
+    return concate( rhs );
+  }
+
+  template<convirtible_to_joint Jointable>
+  friend inline joint operator+ ( joint const &lhs, Jointable const &rhs ) noexcept {
+    return joint( lhs ) += rhs;
+  }
+
+  friend inline constexpr bool operator== ( joint const & lhs, joint const & rhs ) noexcept {
+    return lhs.to_string() == rhs.to_string();
+  }
+  friend inline constexpr bool operator!= ( joint const & lhs, joint const & rhs ) noexcept {
+    return !( lhs == rhs );
+  }
+private:
+  std::string path_element_;
+
+  // 不正な文字列が使われていないかどうかを調べる
+  void isCorrect( std::string const & ) const;
+
+  template <convirtible_to_joint Jointable>
+  joint &concate( Jointable const &rhs ) noexcept;
+};
+
+template<convirtible_to_joint Jointable>
+joint& joint::concate( Jointable const &rhs ) noexcept {
+  path_element_ += PATH_SEPARATOR;
+  if constexpr ( std::is_same_v<Jointable, joint> ) {
+    path_element_ += rhs.path_element_;
+  } else {
+    path_element_ += rhs;
+  }
+  return *this;
+}
 
 } // path
--- src/util/path.cpp
+++ src/util/path.cpp
@@ -1,5 +1,6 @@
 #include "config/config.hpp"
 #include "util/path.hpp"
+#include "util/throw_if.hpp"
 
 #include <vector>
 #include <string>
@@ -25,6 +26,21 @@ std::vector<std::string> recursive_scan_directory( std::string const &scanning_p
   }
 
   return paths;
+}
+
+joint::joint( std::string const & path_element ) : path_element_( path_element ) {
+  isCorrect( path_element_ );
+}
+
+[[deprecated]] void joint::modify( std::function<void ( std::string & )> modify_function ) {
+  modify_function( path_element_ );
+}
+
+void joint::isCorrect( std::string const & path_element ) const {
+  util::throw_if<std::invalid_argument>(
+      (bool)( ( path_element.find( '\0' ) != std::string::npos ) ),
+      "There is prohibited characters"
+  );
 }
 
 }  // path
