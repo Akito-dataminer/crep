8f69c20298bce2da473a476bc4364f8384167429
Akito <102844371+Akito-dataminer@users.noreply.github.com>
2023-08-13 22:40:06 +0900

    modify - throw_if template function to support c++17 (#13)

--- include/util/throw_if.hpp
+++ include/util/throw_if.hpp
@@ -6,15 +6,28 @@
 
 #include <stdexcept>
 #include <string>
-#include <concepts>
 #include <utility>
+
+#include "config/config.hpp"
+#include "util/util.hpp"
 
 namespace util {
 
-template<typename T>
-concept OneOfException = std::is_base_of_v<std::exception, T>;
+#ifdef __cpp_concepts
+template <typename T>
+concept one_of_exception = std::is_base_of_v<std::exception, T>;
+#else
+template <typename T>
+STATIC_CONSTEXPR bool const is_exception = std::is_base_of_v<std::exception, T>;
+#endif
 
-template <OneOfException T>
+#ifdef __cpp_concepts
+#  define TEMPLATE_HEAD_THROW_IF template <one_of_exception T>
+#else
+#  define TEMPLATE_HEAD_THROW_IF template <typename T, util::if_nullp_c<is_exception<T>> * = nullptr>
+#endif
+
+TEMPLATE_HEAD_THROW_IF
 void throw_if( bool const throw_condition, std::string const &error_message ) {
   if ( throw_condition ) {
     throw T( error_message );
