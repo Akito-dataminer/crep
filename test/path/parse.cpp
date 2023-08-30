e37aedeac78d036c11e4768a9949bb2fd297f254
Akito-dataminer <akito.dataminer@gmail.com>
2023-08-30 16:23:04 +0900

    fix - test/path/patse.cpp for Windows

--- test/path/parse.cpp
+++ test/path/parse.cpp
@@ -12,9 +12,16 @@ BOOST_AUTO_TEST_CASE( test_case1 ) {
 
 #if _WIN32
   std::string absolute_path( "C:\\Users\\username\\AppData\\Local\\nvim\\init.lua" );
+  branch_parse parser( absolute_path );
+  BOOST_TEST( parser[0] == "C:" );
+  BOOST_TEST( parser[1] == "Users" );
+  BOOST_TEST( parser[2] == "username" );
+  BOOST_TEST( parser[3] == "AppData" );
+  BOOST_TEST( parser[4] == "Local" );
+  BOOST_TEST( parser[5] == "nvim" );
+  BOOST_TEST( parser[6] == "init.lua" );
 #else
   std::string absolute_path( "/usr/include/c++/11/cstdlib" );
-#endif
 
   branch_parse parser( absolute_path );
   BOOST_TEST( parser[0] == "/" );
@@ -23,6 +30,7 @@ BOOST_AUTO_TEST_CASE( test_case1 ) {
   BOOST_TEST( parser[3] == "c++" );
   BOOST_TEST( parser[4] == "11" );
   BOOST_TEST( parser[5] == "cstdlib" );
+#endif
 }
 
 BOOST_AUTO_TEST_SUITE_END()
