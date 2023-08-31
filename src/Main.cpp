33b429a805b12af623d8a83fbf84c2315081a99e
Akito Dataminer <akito.dataminer@gmail.com>
2023-08-31 19:38:33 +0900

    modify - the way to process errors in main function

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,38 +3,44 @@
 
 #include "config/config.hpp"
 #include "util/path.hpp"
+#include "util/throw_if.hpp"
 
 // std
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <vector>
 #include <utility>
+#include <vector>
 
-int main( int const argc, char const * argv[] ) {
+int main( int const argc, char const *argv[] ) {
   // スケルトンプログラムを保管しているディレクトリを設定する。
-  char const * environment_variable = std::getenv( GET_ENVIRONMENT_VARIABLE );
-  if ( environment_variable == nullptr ) {
-    std::cerr << "The environment variable " << GET_ENVIRONMENT_VARIABLE << " is not able to be read." << std::endl;
-    return -1;
-  }
+  char const *environment_variable = std::getenv( GET_ENVIRONMENT_VARIABLE );
+  util::throw_if<std::runtime_error>(
+      environment_variable == nullptr, std::string( "The environment variable " ) +
+                                           std::string( GET_ENVIRONMENT_VARIABLE ) +
+                                           std::string( "is not able to be read." )
+  );
   std::string skeleton_dir = std::string( environment_variable ) + std::string( APPEND_DIRECTORY );
 
-  bool is_project_skeleton_exist = std::filesystem::is_directory( skeleton_dir );
-
-  if ( !is_project_skeleton_exist ) {
-    std::cerr << "There is NOT project skeleton in " << skeleton_dir << std::endl;
-  }
+  util::throw_if<std::runtime_error>(
+      !std::filesystem::is_directory( skeleton_dir ), std::string( "There is NOT project skeleton in " ) + skeleton_dir
+  );
 
   std::vector<std::string> const original_paths = path::recursive_scan_directory( skeleton_dir );
-  int skeleton_dir_length = skeleton_dir.length() + 1; // skeleton_dirは、末尾が'/'になっていないので、+1しておく
+  int skeleton_dir_length = skeleton_dir.length() + 1;  // skeleton_dirは、末尾が'/'になっていないので、+1しておく
   std::vector<std::pair<std::string, std::string>> source_to_dest;
 
   for ( auto itr : original_paths ) {
     std::string dest_name = itr.substr( skeleton_dir_length );
-    if ( dest_name.substr( 0, 4 ) == ".git" ) { continue; }
-    if ( dest_name.substr( 0, 6 ) == "build/" ) { continue; }
-    if ( dest_name.substr( 0, 7 ) == "LICENSE" ) { continue; }
+    if ( dest_name.substr( 0, 4 ) == ".git" ) {
+      continue;
+    }
+    if ( dest_name.substr( 0, 6 ) == "build/" ) {
+      continue;
+    }
+    if ( dest_name.substr( 0, 7 ) == "LICENSE" ) {
+      continue;
+    }
     source_to_dest.emplace_back( itr, dest_name );
   }
 
