2c486960bede4b2c32f9d0eb045a0621eda494d2
Akito Dataminer <akito.dataminer@gmail.com>
2023-08-31 19:42:00 +0900

    add - try-catch statement

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,44 +13,49 @@
 #include <vector>
 
 int main( int const argc, char const *argv[] ) {
-  // スケルトンプログラムを保管しているディレクトリを設定する。
-  char const *environment_variable = std::getenv( GET_ENVIRONMENT_VARIABLE );
-  util::throw_if<std::runtime_error>(
-      environment_variable == nullptr, std::string( "The environment variable " ) +
-                                           std::string( GET_ENVIRONMENT_VARIABLE ) +
-                                           std::string( "is not able to be read." )
-  );
-  std::string skeleton_dir = std::string( environment_variable ) + std::string( APPEND_DIRECTORY );
+  try {
+    // スケルトンプログラムを保管しているディレクトリを設定する。
+    char const *environment_variable = std::getenv( GET_ENVIRONMENT_VARIABLE );
+    util::throw_if<std::runtime_error>(
+        environment_variable == nullptr, std::string( "The environment variable " ) +
+                                             std::string( GET_ENVIRONMENT_VARIABLE ) +
+                                             std::string( "is not able to be read." )
+    );
+    std::string skeleton_dir = std::string( environment_variable ) + std::string( APPEND_DIRECTORY );
 
-  util::throw_if<std::runtime_error>(
-      !std::filesystem::is_directory( skeleton_dir ), std::string( "There is NOT project skeleton in " ) + skeleton_dir
-  );
+    util::throw_if<std::runtime_error>(
+        !std::filesystem::is_directory( skeleton_dir ),
+        std::string( "There is NOT project skeleton in " ) + skeleton_dir
+    );
 
-  std::vector<std::string> const original_paths = path::recursive_scan_directory( skeleton_dir );
-  int skeleton_dir_length = skeleton_dir.length() + 1;  // skeleton_dirは、末尾が'/'になっていないので、+1しておく
-  std::vector<std::pair<std::string, std::string>> source_to_dest;
+    std::vector<std::string> const original_paths = path::recursive_scan_directory( skeleton_dir );
+    int skeleton_dir_length = skeleton_dir.length() + 1;  // skeleton_dirは、末尾が'/'になっていないので、+1しておく
+    std::vector<std::pair<std::string, std::string>> source_to_dest;
 
-  for ( auto itr : original_paths ) {
-    std::string dest_name = itr.substr( skeleton_dir_length );
-    if ( dest_name.substr( 0, 4 ) == ".git" ) {
-      continue;
+    for ( auto itr : original_paths ) {
+      std::string dest_name = itr.substr( skeleton_dir_length );
+      if ( dest_name.substr( 0, 4 ) == ".git" ) {
+        continue;
+      }
+      if ( dest_name.substr( 0, 6 ) == "build/" ) {
+        continue;
+      }
+      if ( dest_name.substr( 0, 7 ) == "LICENSE" ) {
+        continue;
+      }
+      source_to_dest.emplace_back( itr, dest_name );
     }
-    if ( dest_name.substr( 0, 6 ) == "build/" ) {
-      continue;
+
+    for ( auto itr : source_to_dest ) {
+      if ( std::filesystem::is_directory( itr.first ) ) {
+        std::filesystem::create_directory( itr.second );
+      } else {
+        std::filesystem::copy( itr.first, itr.second );
+      }
     }
-    if ( dest_name.substr( 0, 7 ) == "LICENSE" ) {
-      continue;
-    }
-    source_to_dest.emplace_back( itr, dest_name );
+
+    return 0;
+  } catch ( std::exception &e ) {
+    std::cerr << e.what() << std::endl;
   }
-
-  for ( auto itr : source_to_dest ) {
-    if ( std::filesystem::is_directory( itr.first ) ) {
-      std::filesystem::create_directory( itr.second );
-    } else {
-      std::filesystem::copy( itr.first, itr.second );
-    }
-  }
-
-  return 0;
 }
