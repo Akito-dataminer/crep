34694d8c1829afab7796170c2037853fb6f94240
Akito Dataminer <akito.dataminer@gmail.com>
2023-08-31 13:06:21 +0900

    consolidate - the definition of the path and the list of branches that compose it into one place for testing

--- test/path/branch.cpp
+++ test/path/branch.cpp
@@ -6,6 +6,13 @@
 #include <type_traits>
 
 #define TEST( CONDITION ) BOOST_TEST( ( CONDITION ) == true )
+#if _WIN32
+#  define TEST_PATH "C:\\Users\\username\\AppData\\Local\\nvim\\init.lua"
+#  define BRANCH_LIST "C:", "Users", "username", "AppData", "Local", "nvim", "init.lua"
+#else
+#  define TEST_PATH "/usr/include/c++/11/cstdlib"
+#  define BRANCH_LIST "/", "usr", "include", "c++", "11", "cstdlib"
+#endif
 
 BOOST_AUTO_TEST_SUITE( test_branch )
 
@@ -48,11 +55,7 @@ BOOST_AUTO_TEST_CASE( test_case3 ) {
   using namespace path;
   std::cout << std::endl;
 
-#if _WIN32
-  std::string absolute_path( "C:\\Users\\username\\AppData\\Local\\nvim\\init.lua" );
-#else
-  std::string absolute_path( "/usr/include/c++/11/cstdlib" );
-#endif
+  std::string absolute_path( TEST_PATH );
   branch b( absolute_path );
 
   std::cout << "b.to_string(): " << b.to_string() << std::endl;
