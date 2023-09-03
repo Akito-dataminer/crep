ef826ad000c580f6768a7b005388d509859bf93a
Akito <102844371+Akito-dataminer@users.noreply.github.com>
2023-09-03 23:53:35 +0900

    add - overload of recursive_scan_directory for path::branch (#36)

--- src/util/path.cpp
+++ src/util/path.cpp
@@ -1,9 +1,10 @@
-#include "config/config.hpp"
 #include "util/path.hpp"
-#include "util/throw_if.hpp"
 
-#include <vector>
 #include <string>
+#include <vector>
+
+#include "config/config.hpp"
+#include "util/throw_if.hpp"
 
 namespace path {
 
@@ -28,4 +29,28 @@ std::vector<std::string> recursive_scan_directory( std::string const &scanning_p
   return paths;
 }
 
-}  // path
+void recursive_scan_directory( path::branch const &scanning_path, std::vector<path::branch> &branches ) {
+  for ( const auto &entry :
+        std::filesystem::directory_iterator( static_cast<std::filesystem::path>( scanning_path ) ) ) {
+    branches.emplace_back( entry.path().string() );
+    if ( std::filesystem::is_directory( entry ) ) {
+      recursive_scan_directory( entry.path().string(), branches );
+    }
+  }
+}
+
+std::vector<path::branch> recursive_scan_directory( path::branch const &scanning_path ) {
+  std::vector<path::branch> branches;
+
+  for ( const auto &entry :
+        std::filesystem::directory_iterator( static_cast<std::filesystem::path>( scanning_path ) ) ) {
+    branches.emplace_back( entry.path().string() );
+    if ( std::filesystem::is_directory( entry ) ) {
+      recursive_scan_directory( entry.path().string(), branches );
+    }
+  }
+
+  return branches;
+}
+
+}  // namespace path
