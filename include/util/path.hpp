44d29611f552079e2ae9f637311f9a9b67c1879d
Akito Dataminer <akito.dataminer@gmail.com>
2023-09-03 23:30:42 +0900

    add - overload of recursive_scan_directory for path::branch

--- include/util/path.hpp
+++ include/util/path.hpp
@@ -4,18 +4,20 @@
 
 #pragma once
 
-#include "config/config.hpp"
-#include "util/util.hpp"
-
 #include <functional>
 #include <string>
 #include <type_traits>
 #include <vector>
-#include <type_traits>
+
+#include "config/config.hpp"
+#include "path/branch.hpp"
+#include "util/util.hpp"
 
 namespace path {
 
-void recursive_scan_directory( std::string const &scanning_path, std::vector<std::string> &paths );
+void recursive_scan_directory( std::string const &scanning_path, std::vector<std::string> & );
 std::vector<std::string> recursive_scan_directory( std::string const &scanning_path );
+void recursive_scan_directory( path::branch const &scanning_path, std::vector<path::branch> & );
+std::vector<path::branch> recursive_scan_directory( path::branch const &scanning_path );
 
-} // path
+}  // namespace path
