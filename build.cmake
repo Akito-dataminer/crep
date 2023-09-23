1031cc3e8f64748c71ea1e69ba45f8bb090adb4f
Akito Dataminer <akito.dataminer@gmail.com>
2023-09-23 20:57:00 +0900

    modify - to enable specification of standard version and compiler

--- build.cmake
+++ build.cmake
@@ -1,11 +1,5 @@
 cmake_minimum_required( VERSION 3.22.1 )
 
-set( build_dir ${CMAKE_CURRENT_LIST_DIR}/build )
-
-if( NOT EXISTS ${build_dir} )
-  file( MAKE_DIRECTORY ${build_dir} )
-endif()
-
 if( NOT DEFINED build_type )
   set( build_type Debug )
 endif()
@@ -16,8 +10,23 @@ if( NOT DEFINED slib )
   set( slib libstdc++ )
 endif()
 
+if( NOT DEFINED std_version )
+  set( std_version 20 )
+endif()
+
+if( NOT DEFINED compiler )
+  set( compiler clang++ )
+endif()
+
+set( build_dir ${CMAKE_CURRENT_LIST_DIR}/build/${compiler}/${std_version} )
+message( STATUS "build directory: ${build_dir}" )
+
+if( NOT EXISTS ${build_dir} )
+  file( MAKE_DIRECTORY ${build_dir} )
+endif()
+
 execute_process(
-  COMMAND ${CMAKE_COMMAND} .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -Dslib=${slib}
+  COMMAND ${CMAKE_COMMAND} ../../../ -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -Dslib=${slib} -Dstd_version=${std_version} -Dcompiler=${compiler}
   WORKING_DIRECTORY ${build_dir}
 )
 
