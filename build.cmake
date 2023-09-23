092acdd152c02683efb6ac43cba1458510ab10ef
Akito <102844371+Akito-dataminer@users.noreply.github.com>
2023-09-24 08:20:32 +0900

    Feature/auto test (#48)
    add - the feature to run cmake and ctest with different combinations of compilers and standards

--- build.cmake
+++ build.cmake
@@ -1,11 +1,5 @@
 cmake_minimum_required( VERSION 3.22.1 )
 
-set( build_dir ${CMAKE_CURRENT_LIST_DIR}/build )
-
-if( NOT EXISTS ${build_dir} )
-  file( MAKE_DIRECTORY ${build_dir} )
-endif()
-
 if( NOT DEFINED build_type )
   set( build_type Debug )
 endif()
@@ -16,12 +10,37 @@ if( NOT DEFINED slib )
   set( slib libstdc++ )
 endif()
 
+if( NOT DEFINED std_version )
+  set( std_version 20 )
+endif()
+
+if( NOT DEFINED compiler )
+  set( compiler clang++ )
+endif()
+
+if( NOT DEFINED test )
+  set( build_dir ${CMAKE_CURRENT_LIST_DIR}/build )
+  set( project_root ../ )
+else()
+  set( build_dir ${CMAKE_CURRENT_LIST_DIR}/build_for_test/${compiler}/${std_version} )
+  set( project_root ../../../ )
+endif()
+
+message( STATUS "build directory: ${build_dir}" )
+
+if( NOT EXISTS ${build_dir} )
+  file( MAKE_DIRECTORY ${build_dir} )
+endif()
+
 execute_process(
-  COMMAND ${CMAKE_COMMAND} .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -Dslib=${slib}
+  COMMAND ${CMAKE_COMMAND} ${project_root} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -Dslib=${slib} -Dstd_version=${std_version} -Dcompiler=${compiler}
+  RESULT_VARIABLE is_error_occured
   WORKING_DIRECTORY ${build_dir}
 )
 
-execute_process(
-  COMMAND ${CMAKE_COMMAND} --build . -j8
-  WORKING_DIRECTORY ${build_dir}
-)
+if( ${is_error_occured} EQUAL 0 )
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} --build . -j8
+    WORKING_DIRECTORY ${build_dir}
+  )
+endif()
